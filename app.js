0447ed4db0457b7444ac610e06244e9435eac9b7
Geetesh Singh <geeteshsinghnitian@gmail.com>
2025-12-30 19:06:53 +0530

    Added CREATE & NEW Route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 8080;
 const Listing = require("./models/listing.js");
 const path = require("path");
+const { count } = require("console");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -28,18 +29,37 @@ app.get("/", (req, res) => {
 });
 
 //INDEX ROUTE
-app.get("/listings", async (req, res) => {
+app.get("/listing", async (req, res) => {
   const allListing = await Listing.find({});
   res.render("listings/index.ejs", { allListing });
 });
 
+//NEW ROUTE
+app.get("/listing/new", async (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//CREATE ROUTE
+app.post("/listing", async (req, res) => {
+  // 1. Extract data from the form (req.body)
+  // We can pass req.body directly if input names match Schema keys
+  const newListing = new Listing(req.body);
+
+  // 2. Save to MongoDB
+  await newListing.save();
+  
+  console.log("Data saved successfully!");
+  
+  // 3. Redirect AFTER saving
+  res.redirect("/listing");
+});
+
 //SHOW ROUTE
-app.get("/listings/:id", async (req, res) => {
-  let {id}=req.params;
+app.get("/listing/:id", async (req, res) => {
+  let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
 });
-
 
 // Server started
 app.listen(8080, () => {
